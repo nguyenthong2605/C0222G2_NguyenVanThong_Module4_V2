8988c933c52a67f05ad69b1fe556c4b2d607f84b
unknown <you@example.com>
2022-07-04 16:58:27 +0700

    first commit

---
+++ module4/case_study/case-study-furama/src/main/java/com/codgym/casestudyfurama/model/facility/Facility.java
@@ -0,0 +1,152 @@
+package com.codgym.casestudyfurama.model.facility;
+
+import javax.persistence.*;
+
+@Entity
+public class Facility {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private Integer area;
+
+    private Double cost;
+
+    @Column(name = "max_people")
+    private Integer maxPeople;
+
+    @ManyToOne
+    @JoinColumn(name = "facility_type_id", referencedColumnName = "id")
+    private FacilityType facilityType;
+
+    @ManyToOne
+    @JoinColumn(name = "rent_type_id", referencedColumnName = "id")
+    private RentType rentType;
+
+    private String standardRoom;
+
+    private String descriptionOtherConvenience;
+
+    private Double poolArea;
+
+    private Integer numberOfFloors;
+
+    @Column(columnDefinition = "TEXT")
+    private String facilityFree;
+
+    public Facility() {
+    }
+
+    public Facility(Integer id, String name, Integer area, Double cost, Integer maxPeople, FacilityType facilityType, RentType rentType, String standardRoom, String descriptionOtherConvenience, Double poolArea, Integer numberOfFloors, String facilityFree) {
+        this.id = id;
+        this.name = name;
+        this.area = area;
+        this.cost = cost;
+        this.maxPeople = maxPeople;
+        this.facilityType = facilityType;
+        this.rentType = rentType;
+        this.standardRoom = standardRoom;
+        this.descriptionOtherConvenience = descriptionOtherConvenience;
+        this.poolArea = poolArea;
+        this.numberOfFloors = numberOfFloors;
+        this.facilityFree = facilityFree;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Integer getMaxPeople() {
+        return maxPeople;
+    }
+
+    public void setMaxPeople(Integer maxPeople) {
+        this.maxPeople = maxPeople;
+    }
+
+    public FacilityType getFacilityType() {
+        return facilityType;
+    }
+
+    public void setFacilityType(FacilityType facilityType) {
+        this.facilityType = facilityType;
+    }
+
+    public RentType getRentType() {
+        return rentType;
+    }
+
+    public void setRentType(RentType rentType) {
+        this.rentType = rentType;
+    }
+
+    public String getStandardRoom() {
+        return standardRoom;
+    }
+
+    public void setStandardRoom(String standardRoom) {
+        this.standardRoom = standardRoom;
+    }
+
+    public String getDescriptionOtherConvenience() {
+        return descriptionOtherConvenience;
+    }
+
+    public void setDescriptionOtherConvenience(String descriptionOtherConvenience) {
+        this.descriptionOtherConvenience = descriptionOtherConvenience;
+    }
+
+    public Double getPoolArea() {
+        return poolArea;
+    }
+
+    public void setPoolArea(Double poolArea) {
+        this.poolArea = poolArea;
+    }
+
+    public Integer getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public void setNumberOfFloors(Integer numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
+
+    public String getFacilityFree() {
+        return facilityFree;
+    }
+
+    public void setFacilityFree(String facilityFree) {
+        this.facilityFree = facilityFree;
+    }
+}
